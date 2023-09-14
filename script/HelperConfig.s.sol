cb2709159e8483c07d74dfed7dce1647db1a9225
itachiz91 <badeydev.gm@gmail.com>
2023-09-14 12:16:09 +0100

    first

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/Mockv3Agg.sol";
+
+contract HelperConfig is Script {
+    uint8 public constant DECIMALS = 8;
+    int public constant INITIAL_PRICE = 2000e8;
+    struct NetworkConfig {
+        address priceFeed;
+    }
+    NetworkConfig public activeNetwork;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetwork = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetwork = getMainnetEthConfig();
+        } else {
+            activeNetwork = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetwork.priceFeed != address(0)) {
+            return activeNetwork;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
